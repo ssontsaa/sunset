289ab198ca84c65d035e7e9ee63a38f5aefc67cd
Daria <dariamicovic@gmail.com>
2025-02-06 23:19:48 -0800

    Adjust display size and remove commented code.

--- sunset.js
+++ sunset.js
@@ -3,12 +3,7 @@ var svgN = "http://www.w3.org/2000/svg";
 
 // sets svg object variables
 var canvas = document.getElementById("canvas");
-// var gradient = document.getElementById("myGradient")
-// var sunGrad = gradient.use();
 var sun = document.createElementNS(svgN, "circle");
-var grad_overlay = document.createElementNS(svgN, "rect");
-var gradient = document.createAttributeNS(svgN, "radialGradient");
-
 
 // default canvas specifications and sun attribute 
 var canvasHeight = canvas.getBoundingClientRect().height;
@@ -18,7 +13,7 @@ var canvasCenterX = canvasWidth / 2;
 
 var sunCenterX = canvasCenterX;
 var sunCenterY = canvasCenterY;
-var sunRadius = canvasWidth / 10;
+var sunRadius = canvasHeight / 10;
 var sunColor = "#fcfa79";
 
 // sets default position of the sun at the center of the canvas
@@ -27,27 +22,6 @@ sun.setAttribute("cy", sunCenterY);
 sun.setAttribute("r", sunRadius);
 sun.setAttribute("fill", sunColor);
 
-gradient.setAttribute("cx", sunCenterX);
-gradient.setAttribute("cy", sunCenterY);
-gradient.setAttribute("fr", sunRadius);
-gradient.setAttribute("r", sunRadius * 2);
-
-grad_overlay.setAttribute("x", 0);
-grad_overlay.setAttribute("y", 0)
-grad_overlay.setAttribute("height", canvasHeight);
-grad_overlay.setAttribute("width", canvasWidth);
-grad_overlay.setAttribute("color", "lightgreen");
-grad_overlay.setAttribute("fill", gradient);
-
-// // Create gradient
-// var grad = context.createRadialGradient(150, 75, 15, 150, 75, 150);
-// grad.addColorStop(0, "lightblue");
-// grad.addColorStop(0.3, "pink");
-// grad.addColorStop(1, "darkblue");
-
-var sunsetDurationMS = 3000;
-
-// var sunset = sun.animate();
 
 function resetSun() {
     sunCenterX = canvasCenterX;
@@ -58,17 +32,11 @@ function resetSun() {
 }
 
 function displaySun() {
-    console.log(gradient)
-    console.log(grad_overlay)
-    canvas.appendChild(grad_overlay);
     canvas.appendChild(sun);
 }
 
 function removeSun() {
     canvas.removeChild(sun);
-    // Fill rectangle with gradient
-    // canvas.removeChild(grad_overlay);
-    // context.fillRect(10, 10, 280, 130);
 }
 
 function setSun() {
@@ -80,23 +48,6 @@ function setSun() {
         resetSun();
     }
 }
-
-{/* <animate attributeType="x" from="10" to="100/" dur="5s" fill="freeze" /> */ }
-
-
-
-// function runSunSet() {
-
-//     while (sunCenterY < canvasHeight + sunRadius) {
-//         // requestAnimationFrame(runSunSet);
-//         sunCenterY += 2;
-//         setSunHeight(sunCenterY);
-//         // sun.setAttribute('cy', sunCenterY);
-//     }
-// }
-
-
-
 
 
 function createSunset(duration, from, to) {
@@ -118,11 +69,6 @@ function createSunset(duration, from, to) {
     sunWiden.play();
     resetSun()
 }
-
-// function Move() {
-//     createAnimation("cy", sun, "3s", sun.getAttribute("cy"), canvasCenterY + canvasHeight + sunRadius);
-// }
-
 
 function runSunSet() {
     var duration = 5000; // ms
